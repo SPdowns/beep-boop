cab8b2137a23c3952989a8ef91831a5774b6bced
Sean Downs <spdowns@protonmail.com>
2020-06-12 09:08:40 -0700

    declare a for loop insdie a function in scripts.js and the console log show expected output

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
+function beepBoop(userInput) {
+  const rodgers = ["Beep", "Boop", "Won't You Be My Neighbor"];
+
+  for (let i = 0; i<= userInput; i++)
+  console.log(i)
+
+};
+
 $(document).ready(function() {
   $("form#rodgers-form").submit(function(event) {
     event.preventDefault();
 
     let userInput = $("#input").val()
+    let result = beepBoop(userInput)
 
-    $("#output").text(userInput);
+    $("#output").text(result);
   });
 });
