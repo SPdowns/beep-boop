3c873a24eda5ff708b1f48130facfdbf4d47addb
Sean Downs <spdowns@protonmail.com>
2020-06-12 09:21:02 -0700

    add a variable to convert index to string and match against const rodgers

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,21 @@
 function beepBoop(userInput) {
   const rodgers = ["Beep", "Boop", "Won't You Be My Neighbor"];
-
-  for (let i = 0; i<= userInput; i++)
-  console.log(i)
-
+  let rework = []
+  for (let i = 0; i<= userInput; i++) {
+    console.log(i)
+    let x = i.toString()
+  if (x.includes("3")) {
+    rework.push(rodgers[2]);
+  }
+  return rework
+};
 };
 
 $(document).ready(function() {
   $("form#rodgers-form").submit(function(event) {
     event.preventDefault();
 
-    let userInput = $("#input").val()
+    let userInput = parseInt($("#input").val())
     let result = beepBoop(userInput)
 
     $("#output").text(result);
